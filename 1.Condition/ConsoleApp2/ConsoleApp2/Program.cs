13b790daf5861e5f55624e7699fb05b70225bf19
SabirGuliyev <sabir.guliyev@code.edu.az>
2023-03-03 21:51:47 +0400

    Condition added

---
+++ 1.Condition/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,51 @@
+﻿int age = 18;
+
+
+
+if (age>18)
+{
+    Console.WriteLine("Ishe qebul olundunuz");
+}
+else if (age == 18)
+{
+    Console.WriteLine("Gelen il gelersen");
+}
+else
+{
+    Console.WriteLine("Yashin catmir");
+}
+
+
+if (true)
+{
+
+}
+int choice =4;
+//switch (choice)
+//{
+//    case 1:
+//        Console.WriteLine("Az dili secildi");
+//        break;
+//    case 2 or 3:
+//        Console.WriteLine("Diger diller secildi");
+//        break;
+    
+
+//}
+
+//if(choice == 1)
+//{
+//    Console.WriteLine("Az dili secildi");
+//}
+//else if(choice == 2)
+//{
+//    Console.WriteLine("Rus dili secildi");
+//}
+//else
+//{
+//    Console.WriteLine("Bele bir secimimiz yoxdur");
+//}
+
+
+
+
