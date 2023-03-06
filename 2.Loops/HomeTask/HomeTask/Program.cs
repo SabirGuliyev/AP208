58c37748806b6c9be610c741937ae3ef238d1313
SabirGuliyev <sabir.guliyev@code.edu.az>
2023-03-06 21:39:13 +0400

    Loops added

---
+++ 2.Loops/HomeTask/HomeTask/Program.cs
@@ -0,0 +1,82 @@
+﻿#region Task 1
+//int birth = 2010;
+
+//birth += 18;
+
+//if (birth<=2023)
+//{
+//    Console.WriteLine("Tamam olub");
+//}
+//else
+//{
+//    Console.WriteLine("Tamam olmayib");
+//}
+
+#endregion
+
+
+
+#region Task 2
+//int num = 4123;
+//int sum = 0;
+
+//sum += num % 10;
+//num /= 10;
+//sum += num % 10;
+//num /= 10;
+//sum +=num % 10;
+//num /= 10;
+//sum += num;
+
+//Console.WriteLine(sum); 
+#endregion
+
+
+//for (int i = 0; i < 10; i++)
+//{
+//    Console.WriteLine(i);
+//}
+//for (int i = 0; i < 10; i++)
+//{
+//    Console.WriteLine(i);
+//}
+
+//while (i < 10)
+//{
+//    Console.WriteLine(i);
+//    i++;
+//}
+
+//While
+//bool result = false;
+//string choice = "yes";
+//while (!result)
+//{
+//    Console.WriteLine("Dovr devam etsin?");
+//    choice = Console.ReadLine();
+//    if (choice!="yes")
+//    {
+//        result = true;
+//    }
+
+//}
+//bool result = true;
+//do
+//{
+
+//    Console.WriteLine(result);
+//    Console.WriteLine("Salam sehvdir");
+//} while (result);
+//int i = 0;
+//if (true)
+//{
+//    i = 5;
+//}
+//Console.WriteLine(i);
+
+//for (int i = 10; i > 0; i /= 2)
+//{
+//    i--;
+//    Console.WriteLine(i);
+//}
+
